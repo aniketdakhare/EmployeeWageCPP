35429ab9371e650aa881761fce07293036f0fa9d
“Aniket_D” <aniketdakhare@gmail.com>
2020-08-17 20:10:24 +0530

    ADDED Code to calculate monthly employee wage.

--- EmployeeWageComputation.cpp
+++ EmployeeWageComputation.cpp
@@ -6,27 +6,33 @@ using namespace std;
 int getWorkingHours()
 {
     const int IS_PART_TIME = 1;
-    const int IS_FULL_TIME = 2;   
+    const int IS_FULL_TIME = 2;
+    const int NUM_OF_WORKING_DAYS = 20;    
     int empHrs = 0;
+    int totalEmpHrs = 0;
     srand(time(0));
-    int empCheck = rand() % 3;
-    switch(empCheck)
+    for(int day = 0; day < NUM_OF_WORKING_DAYS; day++)
     {
-        case IS_PART_TIME:
-            empHrs = 4;
-            break;
-        case IS_FULL_TIME:
-            empHrs = 8;
-            break;
-        default:
-            empHrs = 0;
+        int empCheck = rand() % 3;
+        switch(empCheck)
+        {
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
+        }
+        totalEmpHrs += empHrs;
     }
-    return empHrs;
+    return totalEmpHrs;
 }
 
 int main()
 {
     const int EMP_RATE_PER_HOUR = 20;
     int empWage = getWorkingHours() * EMP_RATE_PER_HOUR;
-    cout << "Employee Wage Per Day = " << empWage << endl;    
+    cout << "Employee Wage Per Month = " << empWage << endl;    
 }
