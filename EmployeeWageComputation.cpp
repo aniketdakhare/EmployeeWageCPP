c88ad54d6c4d973a280b7f8d119c786f1969b1da
“Aniket_D” <aniketdakhare@gmail.com>
2020-08-17 19:57:31 +0530

    ADDED Code to calculate employe wage for part time employee.

--- EmployeeWageComputation.cpp
+++ EmployeeWageComputation.cpp
@@ -3,18 +3,30 @@
 #include <ctime>
 using namespace std;
 
+int getWorkingHours()
+{
+    const int IS_PART_TIME = 1;
+    const int IS_FULL_TIME = 2;   
+    int empHrs = 0;
+    srand(time(0));
+    int empCheck = rand() % 3;
+    switch(empCheck)
+    {
+        case IS_PART_TIME:
+            empHrs = 4;
+            break;
+        case IS_FULL_TIME:
+            empHrs = 8;
+            break;
+        default:
+            empHrs = 0;
+    }
+    return empHrs;
+}
+
 int main()
 {
     const int EMP_RATE_PER_HOUR = 20;
-    int empHrs = 0;
-    int empWage = 0;
-    int isPresent = 1;
-    srand(time(0));
-    int empCheck = rand() % 2;
-    if (empCheck == isPresent)
-        empHrs = 8;
-    else
-        empHrs = 0;
-    empWage = empHrs * EMP_RATE_PER_HOUR;
+    int empWage = getWorkingHours() * EMP_RATE_PER_HOUR;
     cout << "Employee Wage Per Day = " << empWage << endl;    
 }
