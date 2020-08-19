982cb286b99ffc941b2ca5bd3ca185fbb4e927fa
“Aniket_D” <aniketdakhare@gmail.com>
2020-08-19 23:30:06 +0530

    ADDED Code to save wages in CSV file.

--- EmployeeWageComputation.cpp
+++ EmployeeWageComputation.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
+#include <fstream>
+
 using namespace std;
+
+vector <int> dailyWorkingHours;
+fstream fileStream;
+
+void writeFile(string file, int empRate)
+{
+    fileStream.open(file, ios::out | ios::trunc);
+    if (fileStream.is_open())
+    {
+        fileStream << "Day, Daily Wage\n";
+        for (int i = 0; i < dailyWorkingHours.size(); i++)
+        {
+            fileStream << "Day_" << (i + 1) << ", "<< (dailyWorkingHours[i] * empRate) << "\n";        
+        }
+    }
+    fileStream.close();
+}
 
 int getWorkingHours()
 {
@@ -9,6 +29,7 @@ int getWorkingHours()
     const int IS_FULL_TIME = 2;
     const int NUM_OF_WORKING_DAYS = 20;
     const int MAX_HRS_IN_MONTH = 100;
+    int empHrs = 0;
     int totalEmpHrs = 0;
     int totalWorkingDays = 0;
     srand(time(0));
@@ -19,14 +40,16 @@ int getWorkingHours()
         switch(empCheck)
         {
             case IS_PART_TIME:
-                totalEmpHrs += 4;
+                empHrs = 4;
                 break;
             case IS_FULL_TIME:
-                totalEmpHrs += 8;
+                empHrs = 8;
                 break;
             default:
-                totalEmpHrs += 0;
+                empHrs = 0;
         }
+        totalEmpHrs += empHrs;
+        dailyWorkingHours.push_back(empHrs);
     }
     return totalEmpHrs;
 }
@@ -35,5 +58,6 @@ int main()
 {
     const int EMP_RATE_PER_HOUR = 20;
     int empWage = getWorkingHours() * EMP_RATE_PER_HOUR;
+    writeFile("EmployeeWageDetails.csv", EMP_RATE_PER_HOUR);
     cout << "Employee Wage Per Month = " << empWage << endl;    
 }
